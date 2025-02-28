70605cb1e1d414b969701e27d34d37e90952f4cc
PavanTaddi9 <taddipavan980@gmail.com>
2025-02-28 12:47:20 +0000

    modified slash to verify

--- codereviewbot/CodereviewbotApp.ts
+++ codereviewbot/CodereviewbotApp.ts
@@ -1,12 +1,25 @@
 import {
     IAppAccessors,
+    IConfigurationExtend,
+    IHttp,
     ILogger,
-} from '@rocket.chat/apps-engine/definition/accessors';
+    IModify,
+    IPersistence,
+    IRead,
+ } from "@rocket.chat/apps-engine/definition/accessors"
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
-
+import { ApiSecurity, ApiVisibility } from "@rocket.chat/apps-engine/definition/api"
+import { UIKitViewSubmitInteractionContext } from "@rocket.chat/apps-engine/definition/uikit"
+import { Analysepr } from "./src/commands/Analysepr";
 export class CodereviewbotApp extends App {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
     }
+    
+     public async extendConfiguration(configuration: IConfigurationExtend) {
+        configuration.slashCommands.provideSlashCommand(new Analysepr())
+  
+        
+     }
 }
